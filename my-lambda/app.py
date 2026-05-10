45d81143ef1efd90b14be599f3ae39af14517498
Harinath Reddy <harinathbliss@gmail.com>
2026-05-10 20:51:13 +0530

    query updated

--- my-lambda/app.py
+++ my-lambda/app.py
@@ -1,5 +1,14 @@
 import json
+from qdrant_client import QdrantClient
+import boto3
 
+client = QdrantClient(
+    host="54.145.235.24",
+    port=6333
+)
+
+collection_name = "pdf_knowledge_base"
+bedrock_runtime = boto3.client(service_name="bedrock-runtime")
 
 
 def lambda_handler(event,context):
@@ -7,11 +16,30 @@ def lambda_handler(event,context):
     request_body = json.loads(event.get('body')) or {}
     user_id = request_body.get('userId')
     query = request_body.get('query')
+    embedding_response = bedrock_runtime.invoke_model(
+            body=json.dumps({"inputText": query}),
+            modelId="amazon.titan-embed-text-v1",
+            accept="application/json",
+            contentType="application/json"
+        )
+    query_vector = json.loads(embedding_response.get("body").read()).get("embedding")
+    
+    print("Query Vector",query_vector)
+
+    results = client.search(
+        query_vector=query_vector,
+        collection_name=collection_name,
+        limit=5,
+        
+    )
 
     print("User Id",user_id)
     print("Query",query)
+
+    print("results",results)
+
     
     return {
     "statusCode": 200,
-    "body": json.dumps({"message": f"User Asked {query} for User Id {user_id}"})
+    "body": json.dumps(results)
     }
