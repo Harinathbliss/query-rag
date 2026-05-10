65124abc43b8961e23a264454f70b4c1bba31f4d
Harinath Reddy <harinathbliss@gmail.com>
2026-05-10 19:16:58 +0530

    Update app.py

--- my-lambda/app.py
+++ my-lambda/app.py
@@ -1,9 +1,16 @@
 import json
 
 
+
 def lambda_handler(event,context):
     print("Lambda Invoked")
+    request_body = event.get('body') or {}
+    user_id = request_body.get('userId')
+    query = request_body.get('query')
+    print("User Id",user_id)
+    print("Query",query)
+    
     return {
     "statusCode": 200,
-    "body": json.dumps({"message": "Success from Stage!"})
+    "body": json.dumps({"message": f"User Asked {query} for User Id {user_id}"})
     }
