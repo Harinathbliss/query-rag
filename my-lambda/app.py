b5b98bc10623f8c9604bde90ff34c21ffdcbe816
Harinath Reddy <harinathbliss@gmail.com>
2026-05-11 21:35:06 +0530

    Update app.py

--- my-lambda/app.py
+++ my-lambda/app.py
@@ -66,7 +66,7 @@ def lambda_handler(event,context):
     })
     
     response = bedrock_runtime.invoke_model(
-    modelId='meta.llama3-70b-instruct-v1:0',
+    modelId='anthropic.claude-3-5-sonnet-20240620-v1:0',
     body=user_prompt
     )
 
